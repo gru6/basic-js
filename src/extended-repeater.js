dd2052c155228477d0822028138864604419863d
gru6 <iliagrushevich@gmail.com>
2022-05-01 22:22:43 +0300

    feat: what-season done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function repeater(str, options) {
+  const { repeatTimes = 1, separator = "+", addition = '', additionRepeatTimes = 1, additionSeparator = "|" } = options;
+  return (str + (addition.split(additionSeparator)) * additionRepeatTimes) * repeatTimes
 }
+console.log(repeater());
 
 module.exports = {
   repeater
