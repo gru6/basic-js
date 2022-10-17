09d1b30364bd95092c6e61e69cfd6098bacb42df
gru6 <iliagrushevich@gmail.com>
2022-10-17 20:56:10 +0300

    feat: task done 80%

--- src/what-season.js
+++ src/what-season.js
@@ -13,11 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   if (date == null) return 'Unable to determine the time of year!'
-  if (Object.keys(date).length > 0 || !(date instanceof Date)) throw new Error('Invalid date!')
+  if (Object.keys(date).length > 0 || !(Object.prototype.toString.call(date) === '[object Date]')) throw new Error('Invalid date!')
   if (date.getMonth() == 11 || date.getMonth() == 0 || date.getMonth() == 1) return "winter";
   if (date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4) return "spring";
   if (date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7) return "summer";
   if (date.getMonth() == 8 || date.getMonth() == 9 || date.getMonth() == 10) return "autumn";
+  console.log(Object.keys(date));
 }
 
 
